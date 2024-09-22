b12ae1ff54e768f9455582b0c37c260ccb106fef
DELL <DELL@DESKTOP-D8M30KI>
2024-09-21 19:26:26 -0500

    ok

--- chat_app/src/main/java/com/chat/app/Configuration/Config.java
+++ chat_app/src/main/java/com/chat/app/Configuration/Config.java
@@ -13,7 +13,7 @@ public class Config implements WebSocketMessageBrokerConfigurer{
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		
-		registry.addEndpoint("/server1").withSockJS();
+		registry.addEndpoint("/server1").setAllowedOrigins("*").withSockJS();
 		
 	}
 
