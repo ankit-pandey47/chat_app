d51ae7a90a614b8cdfbe0191213d20efdb6f0cc8
DELL <DELL@DESKTOP-D8M30KI>
2024-09-21 19:35:54 -0500

    ok

--- chat_app/src/main/java/com/chat/app/Configuration/Config.java
+++ chat_app/src/main/java/com/chat/app/Configuration/Config.java
@@ -2,6 +2,7 @@ package com.chat.app.Configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,7 +14,11 @@ public class Config implements WebSocketMessageBrokerConfigurer{
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		
-		registry.addEndpoint("/server1").setAllowedOrigins("*").withSockJS();
+		//registry.addEndpoint("/server1").setAllowedOrigins("*").withSockJS();
+		
+		 registry.addEndpoint("/server1")
+         .setAllowedOrigins("https://chatapp-production-ankit.up.railway.app")
+         .withSockJS();
 		
 	}
 
@@ -23,6 +28,15 @@ public class Config implements WebSocketMessageBrokerConfigurer{
 		
 		registry.enableSimpleBroker("/topic");
 		registry.setApplicationDestinationPrefixes("/app");
+	}
+
+	
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins("https://chatapp-production-ankit.up.railway.app")
+	            .allowedMethods("GET", "POST", "PUT", "DELETE")
+	            .allowedHeaders("*")
+	            .allowCredentials(true);
 	}
 
 	
